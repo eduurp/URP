bcfdc3411cea66d2221a34108ca806ff40e1a887
MaryDenyaGoyio <wjdakffhgksp@kaist.ac.kr>
2023-09-04 12:49:17 +0900

    Branch_1

--- Bayesian.py
+++ Bayesian.py
@@ -12,21 +12,22 @@ class Belief():
     def __init__(self, correct, dim=4, num=10000):
         self.dim, self.correct = dim, correct # set correct rate function
 
+        self.model = pm.Model()
         self.num, self.samples = num, np.random.multivariate_normal(np.zeros(dim), np.eye(dim), num) # sample by initial Gaussian
-        self.belief = normalize( np.ones(self.num) ) # initial belief, uniform 
         
         self.t, self.theta_hat_t, self.Sigma_t = 1, [np.zeros(dim)], [np.eye(dim)] # record Sigma at each step
 
     def monte_carlo(self, x, y):
-        with pm.Model() as model:
-            mu = pm.Empirical("mu", self.samples)
-            obs = pm.DensityDist("obs", logp=(self.correct if y==1 else 1-self.correct), observed={"theta":mu,"x":x})
+        with self.model:
+            mu = pm.Empirical("mu", self.samples) # 이 부분이 바뀌어야하는데 
+            likelihood = lambda theta : self.correct(theta, x) if y==1 else 1-self.correct(theta, x)
+            obs = pm.Bernoulli('likelihood', p=likelihood, observed=self.samples)
             self.samples = pm.sample()
-        self.model = model
 
     def laplace_approx(self):
-        self.theta_hat = pm.find_MAP(model=self.model, method='L-BFGS-B')
-        self.Sigma = pm.find_hessian(self.theta_hat, vars=[mu])
+        with self.model:
+            self.theta_hat = pm.find_MAP(method='L-BFGS-B')
+            self.Sigma = pm.find_hessian(self.theta_hat, vars=[mu])
 
         self.theta_hat_t.append(self.theta_hat)
         self.Sigma_t.append(self.Sigma)
@@ -38,15 +39,15 @@ class Belief():
         self.laplace_approx()
 
     def imagine_update(self, x, y): # expect next update
-        r_hat = self.correct(self.sample, x) # TODO : theta_hat이 아니라 각 sample의 정답률인 거 같음 -> 어 그러면 belief는 필요가 없어지는데?
-        return normalize( self.belief * (r_hat if y == True else (1-r_hat)) ) # return next belief
+        r_hat = np.array([self.correct(sample, x) for sample in self.samples])
+        return normalize( self.belief * (r_hat if y == True else (1-r_hat)) ) # 이 부분이 Belief로 바뀔듯
 
     def update(self, x, y): # realization of update
         self.belief = self.imagine_update(x, y)
         self.monte_carlo(x, y)
         self.laplace_approx()
 
-def linear(theta, x):
+def linear(theta, x): # TODO : 여러 개 들어오면 여러개 내 보내줘야함 or r_hat이 list이거나
     return sigmoid(theta.T@x)
 
 Belief_test = Belief(linear)
