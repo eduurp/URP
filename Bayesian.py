b284fb865d9fdc5419a71b539f694b00944ce352
MaryDenyaGoyio <wjdakffhgksp@gmail.com>
2023-09-08 01:23:00 +0900

    230908_1

--- Bayesian.py
+++ Bayesian.py
@@ -3,37 +3,34 @@ import numpy as np
 import pandas as pd
 
 from scipy.optimize import minimize
-from sklearn.neighbors import KernelDensity
+from scipy.stats import multivariate_normal
 
 def normalize(belief):   return belief/sum(belief)
 def sigmoid(x): return 1 / (1 + np.exp(-x))
 
-
 class Belief():
-    def __init__(self, correct, dim=4, num=10000, verbose=False):
-        self.correct, self.dim, self.num = correct, dim, num # set correct rate function
+    def __init__(self, correct, dim=4, verbose=False):
         self.verbose = verbose
+        self.correct, self.dim = correct, dim # set correct rate function
         self.initialize()
 
     def initialize(self): # clear history
-        self.samples = np.random.multivariate_normal(np.zeros(self.dim), np.eye(self.dim), self.num) # sample by initial Gaussian
-        self.kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(self.samples)
-
         self.theta_hat, self.hessian_inv = np.zeros(self.dim), np.eye(self.dim)
-        self.t, self.theta_hat_t, self.hessian_inv_t = 1, [np.zeros(self.dim)], [np.eye(self.dim)] # record hessian_inv at each step
+        self.t, self.theta_hat_t, self.hessian_inv_t = 1, [self.theta_hat], [self.hessian_inv]
+        
+        self.prior = lambda theta : multivariate_normal.pdf(theta, mean=self.theta_hat, cov=self.hessian_inv)
+        self.log_likelihood_t = [self.prior] # record hessian_inv at each step
+        self.neg_log_posterior = lambda theta : -sum(log_likelihood(theta) for log_likelihood in self.log_likelihood_t)
 
         self.explain()
 
     def bayesian_update(self, x, y):
-        # KDE sampling
+        # Log Likelihood
         log_likelihood = lambda theta : np.log( self.correct(theta, x) if y==1 else 1-self.correct(theta, x) )
-        neg_log_posterior = lambda theta : -( log_likelihood(theta) + self.kde.score_samples(theta.reshape(1, -1)) )
-        
-        self.samples = self.kde.sample(self.num) # resampling
-        self.kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(self.samples)
+        self.log_likelihood_t.append(log_likelihood)
 
         # Laplace Approximation
-        opt_result = minimize(neg_log_posterior, self.theta_hat)
+        opt_result = minimize(self.neg_log_posterior, self.theta_hat)
         self.theta_hat = opt_result.x
         self.hessian_inv = opt_result.hess_inv
 
@@ -57,5 +54,6 @@ def linear(theta, x):
     return sigmoid(theta.T@x)
 
 Belief_test = Belief(linear, verbose=True)
-Belief_test.bayesian_update(np.ones(4), 1)
-
+Belief_test.bayesian_update(np.ones(4)/1000, 0)
+Belief_test.bayesian_update(np.ones(4)/1000, 1)
+Belief_test.bayesian_update(np.ones(4)/1000, 0)
