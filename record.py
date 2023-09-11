d58c04379a0568168a453f1bfd1e73caefd944de
MaryDenyaGoyio <wjdakffhgksp@gmail.com>
2023-09-11 23:41:05 +0900

    230911_13

--- record.py
+++ record.py
@@ -8,17 +8,18 @@ def make_answers(num_t, num_seed, seed=0):
     np.save(f"answers_{num_t}_{num_seed}_{max(numbers, default=0) + 1}.npy", answers)
 
 class Exam(Query):
-    def __init__(self, query, correct, dim, true_theta, verbose=1, file_num=None):
+    def __init__(self, query, correct, dim, true_theta, about, verbose=1, file_num=None):
         file_dict = {int(re.search(r"answers_\d+_\d+_(\d+).npy", f).group(1)): f for f in os.listdir('.') if re.match(r"answers_\d+_\d+_(\d+).npy", f)}
         self.file_num = max(file_dict.keys()) if not file_num else file_num
+        self.about = about
 
         super().__init__(query, correct, dim, true_theta, np.load(file_dict[self.file_num]), verbose)
 
     def initialize(self, query, true_theta):
         super().initialize(query, true_theta)
 
-        numbers = [int(re.search(rf"exam_{self.num_t}_{self.num_seed}_{self.file_num}_(\d+)", f).group(1)) for f in os.listdir('.') if re.search(rf"exam_{self.num_t}_{self.num_seed}_{self.file_num}_(\d+)", f)]
-        self.folder = f"exam_{self.num_t}_{self.num_seed}_{self.file_num}_{max(numbers, default=0) + 1}"
+        numbers = [int(re.search(rf"exam_{self.num_t}_{self.num_seed}_{self.file_num}_(\d+)_.*", f).group(1)) for f in os.listdir('.') if re.search(rf"exam_{self.num_t}_{self.num_seed}_{self.file_num}_(\d+)_.*", f)]
+        self.folder = f"exam_{self.num_t}_{self.num_seed}_{self.file_num}_{max(numbers, default=0) + 1}_{self.about}"
         os.makedirs(self.folder, exist_ok=True)
 
         np.save(os.path.join(self.folder, "true_theta.npy"), self.true_theta)
