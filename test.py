f44f1f6866098ce960987304e42b8ab1f260cdfc
MaryDenyaGoyio <wjdakffhgksp@gmail.com>
2023-09-11 14:05:12 +0900

    230911_4

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ def ones_query(Belief):
 def linear(theta, x):
     return sigmoid(theta.T@x)
 
+def 
+    val,vec = np.linalg.eig(Belief_test.neg_hessian)
+    query = np.insert(query_bound_norm * vec[-1], len(vec[-1]),-query_bound_norm * vec[-1].T@Belief_test.theta_hat)
+
 Test = Exam(ones_query, linear, 4, np.ones(4), verbose=1)
 Test.exam()
 Test.save()
