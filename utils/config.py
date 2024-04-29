7b394d3312a0a7c767d573f56ba465d5580141c6
MaryDenyaGoyio <wjdakffhgksp@gmail.com>
2024-04-30 07:49:06 +0900

    240430_Detail

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-ANSWER_SHEET = "answers.npy"
-TRUE_THETA = "true_thetas.npy"
-INIT_NHESS = "init_nhesses.npy"
+ANSWER_SHEET = "answers_10_1000.npy"
+TRUE_THETA = "true_thetas_10_4.npy"
+INIT_NHESS = "init_nhesses_100_4_4.npy"
 VERBOSE = 4
