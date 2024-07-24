e2a690537b49ae488388c7830ce827c1dd5775fd
eduurp <142292286+eduurp@users.noreply.github.com>
2024-07-24 18:24:16 +0900

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -14,3 +14,18 @@ def create_directory(directory_name):
         print(f"Directory already existed : {directory_name}")
     except Exception as e:
         print(f"Erorr occured : {e}")
+
+def return_PQI(R) :
+    obs = ~np.isnan(R)
+    possible_query_idx = []
+    for i in range(obs.shape[0]):
+        true_indices = np.where(obs[i])
+        possible_query_idx.append(true_indices[0])
+            
+    PQIs = []
+
+    for i in range(len(possible_query_idx)) :
+        PQIs.append(possible_query_idx[i].tolist())
+    PQI = PQIs
+    max_query_num = np.min([np.sum(obs[j]) for j in range(len(obs))])
+    return PQI, max_query_num
