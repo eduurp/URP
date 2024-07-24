39bf66e92e6ffd77bc2cd7a848246cb12ad877e5
eduurp <142292286+eduurp@users.noreply.github.com>
2024-07-24 18:25:53 +0900

    Update test_realworld.py

--- test_realworld.py
+++ test_realworld.py
@@ -18,44 +18,28 @@ else :
 
 Algo_name = "BALD"
 
-Algo_list = [BALD, DURM]
-Algo_name_list = ["BALD", "DURM"]
+Algo_list = [BALD, DURM, MaxEnt, VarRatio, MeanStd]
+Algo_name_list = ["BALD", "DURM", "MaxEnt", "VarRatio", "MeanStd"]
 algorithm = Algo_list[Algo_name_list.index(Algo_name)]
 
 dim = 4
 
-
 R = np.load("./params_realworld/" + model_name+ "_Dataset.npy")
 P = np.load("./params_realworld/Theta_" +  model_name + "_" + str(dim) + "_MSE.npy")
 Q = np.load("./params_realworld/Query_" +  model_name + "_" + str(dim) + "_MSE.npy")
+PQI, max_query_num = return_PQI(R)
 
-obs = ~np.isnan(R)
-possible_query_idx = []
-for i in range(obs.shape[0]):
-    true_indices = np.where(obs[i])
-    possible_query_idx.append(true_indices[0])
-        
-PQIs = []
-
-for i in range(len(possible_query_idx)) :
-    PQIs.append(possible_query_idx[i].tolist())
-PQI = PQIs
-
-max_query_num = np.min([np.sum(obs[j]) for j in range(len(obs))])
 num_t = 10 # num_t <= max_query_num
-
 
 MAP_0 = np.mean(P,axis = 0)
 nHess_0 = np.linalg.inv(np.cov(P.T))
 
 # num_population <= len(P)
 exp = Experiment_Realworld(algorithm, dim, model, num_t, MAP_0, nHess_0, R, P, Q, PQI)
-
 current_time = datetime.datetime.now().strftime('%y%m%d-%H-%M')
 create_directory(current_time)
 
-pop_num = 10
-# pop_num < len(P)
+pop_num = 10 # pop_num < len(P)
 for i in range(pop_num) :
     print(len(PQI[i]))
     print(Q[PQI[i]].shape)
